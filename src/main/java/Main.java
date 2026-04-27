901c80ff3da8c3750df41e0379b8f49700733572
Audrey Burke <burkea17@xavier.edu>
2026-04-27 17:20:40 -0400

    add songs to four other genre ArrayLists

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,10 @@ public class Main {
         ArrayList<String> popSongs = new ArrayList<>();
         ArrayList<String> showTunesSongs = new ArrayList<>();
         ArrayList<String> indieSongs = new ArrayList<>();
+        ArrayList<String> rnbSongs = new ArrayList<>();
+        ArrayList<String> hiphopSongs = new ArrayList<>();
+        ArrayList<String> jazzSongs = new ArrayList<>();
+        ArrayList<String> classicalSongs = new ArrayList<>();
 
         genres.add("Pop");
         genres.add("R&B");
@@ -30,6 +34,24 @@ public class Main {
         indieSongs.add("Earrings by Malcolm Todd");
         indieSongs.add("Calling After Me by Wallows");
         indieSongs.add("No Use by Christian Leave");
+
+        rnbSongs.add("Snooze by SZA");
+        rnbSongs.add("Lost by Frank Ocean");
+        rnbSongs.add("Heartbreak Anniversary by Giveon");
+
+        hiphopSongs.add("Bodak Yellow by Cardi B");
+        hiphopSongs.add("HUMBLE. by Kendrick Lamar");
+        hiphopSongs.add("Keep Ya Head Up by 2Pac");
+
+        jazzSongs.add("From the Start by Laufey");
+        jazzSongs.add("Fly Me to the Moon by Frank Sinatra and Count Basie");
+        jazzSongs.add("A Lovely Night from La La Land");
+
+        classicalSongs.add("Clair de Lune by Debussy");
+        classicalSongs.add("Moonlight Sinata by Beethoven");
+        classicalSongs.add("The Four Seasons by Vivaldi");
+
+
 
         Random random = new Random();
         String randomGenre = genres.get(random.nextInt(genres.size()));
